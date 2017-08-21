f4921d7c38ba45087ce4d35cd5ff53d3130d22db
hubaka <aillanan@yahoo.com>
2017-08-21 16:25:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,17 @@ image.onclick = function() {
 var counter = 0;
 var buttonclick = document.getElementById("counter");
 buttonclick.onclick = function() {
-    counter += 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    
+    var request = XMLHttpRequest();
+    request.open('GET', 'http://aillanan.imad.hasura-app.io/counter', true);
+    request.send(null);
+    request.onreadystatechange = function() {
+        if (request.readystate === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                    var counter = request.responseText;
+                    var span = document.getElementById("count");
+                    span.innerHTML = counter.toString();
+            }
+        }
+    }
 }
