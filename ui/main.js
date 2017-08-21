ad20e1e1968210c983812a9934f4be7b2ca17fdf
hubaka <aillanan@yahoo.com>
2017-08-21 14:11:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,10 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "A new value";
 
+
+// going to make the image move on click
+var image = document.getElementById("madipic");
+
+image.onClick = function() {
+   image.style.marginLeft = '100px'; 
+};
