53425090887ddd72442cf1210ba8cd6c01cc6411
hubaka <aillanan@yahoo.com>
2017-08-21 15:52:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 
 
 // chanding the main text printed in the page
-var element = document.getElementById("main-text");
-element.innerHTML = "A new value";
+// var element = document.getElementById("main-text");
+// element.innerHTML = "A new value";
 
 
 // going to make the image move on click
